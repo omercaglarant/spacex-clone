71e05dcccc0eba8d576ee98d3a89f03758ca4037
Ömer Çağlar <omercaglarant@gmail.com>
2022-11-17 23:25:55 +0300

    counter are created

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
 
 
 btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
+
+
+function scrollPage() {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100 && !scrollStarted) {
+        countUp();
+        scrollStarted = true;
+    } else if (scrollPosition < 100 && scrollStarted) {
+        reset();
+        scrollStarted = false;
+    }
+}
+
+
 function navToggle() {
     btn.classList.toggle('open');
     overlay.classList.toggle('overlay-show');
@@ -11,3 +29,30 @@ function navToggle() {
     menu.classList.toggle('show-menu');
 }
 
+function countUp() {
+    counters.forEach((counter) => {
+        counter.innerText = "0";
+
+        const updateCounter = () => {
+            
+            const target = +counter.getAttribute('data-target');
+            
+            const c = +counter.innerText;
+            
+            const incerement = target / 100;
+
+            if(c < target) {
+                counter.innerText = `${Math.ceil(c + incerement)}`;
+                setTimeout(updateCounter, 75);
+            } else {
+                counter.innerText = target;
+            }
+
+        };
+        updateCounter();
+    });
+}
+
+function reset() {
+    counters.forEach((counter) => counter.innerHTML = "0");
+}
